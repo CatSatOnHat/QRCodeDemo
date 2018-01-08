c8547f92ef99bfba073992c4a25d69298f21ca49
李朕 <Dan@sensology.com.cn>
2018-01-08 11:05:58 +0800

    add image scan

--- QRCodeDemo/ViewController.m
+++ QRCodeDemo/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import "QRCodeVC.h"
 
-@interface ViewController ()
+@interface ViewController () <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UILabel *scanResult;
 
@@ -29,6 +29,32 @@
     };
     [self presentViewController:scanVC animated:YES completion:nil];
 }
+
+- (IBAction)GetQRCodeInfoFromImage:(id)sender {
+    UIImagePickerController *pickController = [[UIImagePickerController alloc] init];
+    pickController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    pickController.delegate = self;
+    [self presentViewController:pickController animated:YES completion:nil];
+}
+#pragma mark - UIImagePickerControllerDelegate
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    NSString *type = [info objectForKey:UIImagePickerControllerMediaType];
+    if ([type isEqualToString:@"public.image"]) {
+        UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+        [picker dismissViewControllerAnimated:YES completion:^{
+            
+            CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeQRCode context:nil options:@{CIDetectorAccuracy:CIDetectorAccuracyHigh}];
+            CIImage *QRCodeImage = [[CIImage alloc] initWithImage:image];
+            NSArray *features = [detector featuresInImage:QRCodeImage];
+            if (features.count > 0) {
+                _scanResult.text = [[features firstObject] messageString];
+            } else {
+                _scanResult.text = @"照片中未识别到二维码";
+            }
+        }];
+    }
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
